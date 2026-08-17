eaf191ac874ab54b9b93ffb0b5c35f1c94d13eb0
Ronald Castro <ronaldcastroapp@gmail.com>
2026-08-16 23:55:27 -0500

    Definir eventos base de DexRelay

---
+++ app/core/events.py
@@ -0,0 +1,10 @@
+class Events:
+    GAME_CONNECTED = "GameConnected"
+    GAME_DISCONNECTED = "GameDisconnected"
+
+    POKEMON_CHANGED = "PokemonChanged"
+    POKEMON_EVOLVED = "PokemonEvolved"
+    POKEMON_FAINTED = "PokemonFainted"
+
+    BADGE_OBTAINED = "BadgeObtained"
+    TEAM_UPDATED = "TeamUpdated"
